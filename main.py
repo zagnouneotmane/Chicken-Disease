8c214fb31e4204a1dc0fcf3ff740a57188f19462
zagnouneotmane <zagnoune.otmane@gmail.com>
2023-06-16 11:06:06 +0100

    prepare Callbacks

--- main.py
+++ main.py
@@ -27,3 +27,14 @@ except Exception as e:
         raise e
 
 
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
