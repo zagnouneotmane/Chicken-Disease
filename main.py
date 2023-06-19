3caa786895f32587c4d85f7198eb87b884adeef2
zagnouneotmane <zagnoune.otmane@gmail.com>
2023-06-19 20:28:00 +0100

    training

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ChickenClassifier import logger
 from ChickenClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ChickenClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from ChickenClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -34,6 +34,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/ChickenClassifier/config/configuration.py
+++ src/ChickenClassifier/config/configuration.py
@@ -2,9 +2,10 @@ from ChickenClassifier.constants import *
 import os
 from pathlib import Path
 from ChickenClassifier.utils.common import read_yaml, create_directories
-from ChickenClassifier.entity.config_entity import (DataIngestionConfig, 
-                                                    PrepareBaseModelConfig,
-                                                    PrepareCallbacksConfig)
+from ChickenClassifier.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig,
+                                                PrepareCallbacksConfig,
+                                                TrainingConfig)
 
 
 
@@ -34,25 +35,30 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
-
-    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
-            config = self.config.prepare_base_model
-            
-            create_directories([config.root_dir])
-
-            prepare_base_model_config = PrepareBaseModelConfig(
-                root_dir=Path(config.root_dir),
-                base_model_path=Path(config.base_model_path),
-                updated_base_model_path=Path(config.updated_base_model_path),
-                params_image_size=self.params.IMAGE_SIZE,
-                params_learning_rate=self.params.LEARNING_RATE,
-                params_include_top=self.params.INCLUDE_TOP,
-                params_weights=self.params.WEIGHTS,
-                params_classes=self.params.CLASSES
-            )
-
-            return prepare_base_model_config
     
+
+
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
+    
+
+
     def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
         config = self.config.prepare_callbacks
         model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
@@ -68,3 +74,30 @@ class ConfigurationManager:
         )
 
         return prepare_callback_config
+    
+
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chicken-fecal-images")
+        create_directories([
+            Path(training.root_dir)
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
+    
+
+      
